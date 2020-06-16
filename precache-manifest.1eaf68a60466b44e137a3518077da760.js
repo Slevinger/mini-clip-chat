559906eb252c83155f3888884c49f60ae5cfa271
slevinger <shir.levinger@gmail.com>
2020-06-16 15:58:11 +0300

    Updates

---
+++ precache-manifest.1eaf68a60466b44e137a3518077da760.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85094e9d81e9d585fd225c44d351a856",
+    "url": "/mini-clip-chat/index.html"
+  },
+  {
+    "revision": "e63002bb62010cdf9779",
+    "url": "/mini-clip-chat/static/css/main.1f4ea31b.chunk.css"
+  },
+  {
+    "revision": "24e85fce734ce890e094",
+    "url": "/mini-clip-chat/static/js/2.da11124e.chunk.js"
+  },
+  {
+    "revision": "3be25bf51018a491ef7f73dfd2dd74c4",
+    "url": "/mini-clip-chat/static/js/2.da11124e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e63002bb62010cdf9779",
+    "url": "/mini-clip-chat/static/js/main.d6ffd884.chunk.js"
+  },
+  {
+    "revision": "557a56039022903ed9af",
+    "url": "/mini-clip-chat/static/js/runtime-main.e0e1a922.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0e69529d83425ae92cc896c5183f7675.js"
+  "/mini-clip-chat/precache-manifest.1eaf68a60466b44e137a3518077da760.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/mini-clip-chat/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
