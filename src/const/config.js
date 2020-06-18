816bc1b25d71a5c6fc4aa3528256cfb39950e439
slevinger <shir.levinger@gmail.com>
2020-06-18 10:20:05 +0300

    change server path to any-clip

--- src/const/config.js
+++ src/const/config.js
@@ -1,5 +1,5 @@
 export const PORT = 3000;
 export const DEV_PATH = `http://localhost:${PORT}`;
-export const PROD_PATH = `https://mini-clip-server.herokuapp.com/`;
+export const PROD_PATH = `https://any-clip-server.herokuapp.com/`;
 
 export const url = PROD_PATH;
